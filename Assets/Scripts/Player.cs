d53c93cd274a78df737d3c142222346e55b3ff5c
ACG <anandaguneratne@gmail.com>
2016-10-27 09:50:47 -0500

    Health bar implemented
    Still a few bugs to work out

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,10 @@ public class Player : Photon.MonoBehaviour {
 
 	private Vector3 targetPos;
 	public float movespeed = 5.0f;
-	public float maxHealth = 10.0f;
-	private float health;
+
 	// Use this for initialization
 	void Start () {
-		health = maxHealth;
+		
 	}
 	
 	// Update is called once per frame
@@ -20,14 +19,11 @@ public class Player : Photon.MonoBehaviour {
 			if (Input.GetMouseButton (1)) {
 				targetPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 				targetPos.z = transform.position.z;
-				Debug.Log (targetPos);
 			}
 			//Move toward target location
 			transform.position = Vector3.MoveTowards (transform.position, targetPos, movespeed * Time.deltaTime);
 		}
-		//debug hp
-		health -= Time.deltaTime;
-		if (health < 0)
-			health = maxHealth; 
 	}
+
+
 }
